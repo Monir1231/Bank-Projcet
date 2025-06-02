e02ec8a6dfce4d4412b7636ea35ba3add9d8a226
Monir1231 <monirmolla324@gmail.com>
2025-06-02 22:32:24 +0600

    bank-site

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-// vite.config.js
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: './', // static hosting এর জন্য দরকার হতে পারে
-})
+export default defineConfig ({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                careers: resolve(__dirname, "careers.html"),
+                about: resolve(__dirname, "about.html"),
+                 login: resolve(__dirname, "login.html"),
+                security: resolve(__dirname, "security.html"),
+                singup: resolve(__dirname, "singup.html")
+            },
+        },
+    },
+});
